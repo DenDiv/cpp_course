61ec08b5927159a15cdf94136ef7815a9049545a
Denis Divitskii <D.Divitsky.psa@skoltech.ru>
2023-01-13 14:58:26 +0300

    added header defines

--- prime_numbers_lib/FindPrimeNth.h
+++ prime_numbers_lib/FindPrimeNth.h
@@ -1,2 +1,14 @@
+#ifndef FINDPRIMENTH_H
+#define FINDPRIMENTH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 unsigned long long FindPrimeDummy(int n);
 unsigned long long FindPrimeErat(int n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
