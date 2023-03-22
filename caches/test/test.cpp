83f8ecfb285f35a20bb538ab62b47c7e0071db6a
Denis Divitskii <D.Divitsky.psa@skoltech.ru>
2023-03-22 13:20:55 +0300

    finished caches task

--- caches/test/test.cpp
+++ caches/test/test.cpp
@@ -22,7 +22,7 @@ TEST(CacheTests, LRU_cache)
     std::string line, ans;
     size_t line_counter = 0;
 
-    std::string data_file_path = std::string(TEST_DATA_PATH) + "/test_data_lru.dat";
+    std::string data_file_path = std::string(TEST_DATA_PATH) + "/test_data.dat";
     std::string ans_file_path = std::string(TEST_DATA_PATH) + "/test_data_lru.ans";
 
     datafile.open(data_file_path, std::ios::in);
@@ -42,6 +42,51 @@ TEST(CacheTests, LRU_cache)
 
         ss >> m >> n;
         caches::lru_cache<page_t, int> c{m};
+        for (int i = 0; i < n; ++i)
+        {
+            int q;
+            ss >> q;
+            if (c.lookup_update(q, slow_get_page))
+                hits++;
+        }
+
+        std::getline(ansfile, ans);
+        if (std::stoi(ans) != hits)
+        {
+            std::cout << "ans isn't correct, line: " << line_counter;
+            ASSERT_TRUE(0);
+        }
+        line_counter++;
+    }
+    ASSERT_TRUE(1);
+}
+
+TEST(CacheTests, Two_q_simplified_cache)
+{
+    std::fstream datafile, ansfile;
+    std::string line, ans;
+    size_t line_counter = 0;
+
+    std::string data_file_path = std::string(TEST_DATA_PATH) + "/test_data.dat";
+    std::string ans_file_path = std::string(TEST_DATA_PATH) + "/test_data_2q.ans";
+
+    datafile.open(data_file_path, std::ios::in);
+    ansfile.open(ans_file_path, std::ios::in);
+
+    if (!datafile.is_open() || !ansfile.is_open())
+    {
+        throw std::runtime_error("Could not open file");
+    }
+
+    while (std::getline(datafile, line))
+    {
+        std::stringstream ss(line);
+        int hits = 0;
+        size_t m;
+        int n;
+
+        ss >> m >> n;
+        caches::two_q_simple_cache<page_t, int> c{m};
         for (int i = 0; i < n; ++i)
         {
             int q;
