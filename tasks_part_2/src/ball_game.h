148647d7209829c5f11247239867263146ab5d00
Denis Divitskii <D.Divitsky.psa@skoltech.ru>
2023-04-14 14:48:32 +0300

    updated ball game

--- tasks_part_2/src/ball_game.h
+++ tasks_part_2/src/ball_game.h
@@ -1,35 +1,123 @@
 #include <algorithm>
-#include <iostream>
 #include <cassert>
+#include <iostream>
 #include <math.h>
 #include <mutex>
+#include <string>
 
-class Ball final
+
+class BallVirt
 {
-private:
+protected:
     double x_;
     double y_;
     double xv_;
     double yv_;
     double t_;
-    std::mutex mt_;
-public:
-    Ball(double x, double y, double xv, double yv, double t = 0.0): x_(x), y_(y), xv_(xv), yv_(yv), t_(t) {}
 
-    void fly(double t)
+public:
+    BallVirt(double x, double y, double xv, double yv, double t = 0.0) : x_(x), y_(y), xv_(xv), yv_(yv), t_(t) {}
+    virtual ~BallVirt() = 0 {}
+    virtual void print() = 0 { std::cout << "x: " << x_ << " y: " << y_ << " xv: " << xv_ << " yv: " << yv_ << " t: " << t_ << std::endl; }
+    virtual void fly(double t) = 0;
+    virtual void push(double a_v, double a_alpha) = 0;
+
+public:
+    // common functions
+    std::vector<double> get_coords() const { return std::vector<double>{x_, y_}; }
+    std::vector<double> get_velocity() const { return std::vector<double>{xv_, yv_}; }
+    double get_time() const { return t_; }
+};
+
+class BallSimple final : public BallVirt
+{
+private:
+    double r_;
+    double weight_;
+    std::mutex mt_;
+
+public:
+    BallSimple(double r, double weight, double x, double y, double xv, double yv, double t = 0.0)
+        : BallVirt(x, y, xv, yv, t), r_(r), weight_(weight)
+    {}
+
+    ~BallSimple() {}
+
+    void fly(double t) override
     {
         assert(t > 0);
-        std::lock_guard<std::mutex> lg(mt_); 
+        std::lock_guard<std::mutex> lg(mt_);
         x_ += xv_ * t;
         y_ += yv_ * t;
         t_ += t;
     }
 
-    void push(double a_v, double a_alpha)
+    void push(double a_v, double a_alpha) override
     {
         assert(a_v > 0);
-        std::lock_guard<std::mutex> lg(mt_); 
+        std::lock_guard<std::mutex> lg(mt_);
         xv_ += a_v * cos(a_alpha);
         yv_ += a_v * sin(a_alpha);
     }
+
+    void print() override
+    {
+        std::lock_guard<std::mutex> lg(mt_);
+        std::cout << "Thread ID: " << std::this_thread::get_id() << std::endl;
+        std::cout << "r: " << r_ << " weight: " << weight_ << std::endl;
+        BallVirt::print();
+    }
+};
+
+class BallWind final : public BallVirt
+{
+private:
+    double r_;
+    double weight_;
+    double wind_k_;  // same for x and y wind force koef
+    std::mutex mt_;
+
+public:
+    BallWind(double r, double weight, double wind_speed, double x, double y, double xv, double yv, double t = 0.0)
+        : BallVirt(x, y, xv, yv, t), r_(r), weight_(weight), wind_k_(wind_speed)
+    {}
+
+    ~BallWind() {}
+
+    void fly(double t) override
+    {
+        assert(t > 0);
+        std::lock_guard<std::mutex> lg(mt_);
+        x_ += wind_k_ * xv_ * t;
+        y_ += wind_k_ * yv_ * t;
+        t_ += t;
+    }
+
+    void push(double a_v, double a_alpha) override
+    {
+        assert(a_v > 0);
+        std::lock_guard<std::mutex> lg(mt_);
+        xv_ += a_v * cos(a_alpha);
+        yv_ += a_v * sin(a_alpha);
+    }
+
+    void print() override
+    {
+        std::lock_guard<std::mutex> lg(mt_);
+        std::cout << "Thread ID: " << std::this_thread::get_id() << std::endl;
+        std::cout << "r: " << r_ << " weight: " << weight_ << " wind_k: " << wind_k_ << std::endl;
+        BallVirt::print();
+    }
+};
+
+class Player final
+{
+public:
+    Player(std::string name = "huilack") { name_ = name; }
+
+    void push_ball(BallVirt& b, double a_v, double a_alpha) { b.push(a_v, a_alpha); }
+
+    void fly_ball(BallVirt& b, double t) { b.fly(t); }
+
+    std::string name_;
 };
--- tasks_part_2/test/test.cpp
+++ tasks_part_2/test/test.cpp
@@ -4,34 +4,51 @@
 #include <fstream>
 #include <gtest/gtest.h>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <thread>
 
 
-#define private public
 #include <Config.h>
 #include <ball_game.h>
 
 constexpr float eps = 0.0001f;
+constexpr double PI = 3.1415926535897932384626;
 
-void play_ball(Ball& b)
+void play_ball(Player& p, std::vector<std::shared_ptr<BallVirt>>& balls)
 {
-    std::cout << "start play thread id: " << std::this_thread::get_id() << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    b.fly(10);
-    std::cout << "end play thread id: " << std::this_thread::get_id() << std::endl;
+    for (auto& b : balls)
+    {
+        b->push(1.0, PI / 4);
+        b->fly(1.0);
+        b->print();
+    }
 }
 
-TEST(FuncTests, ball_game_simple)
+TEST(FuncTests, ball_game)
 {
-    Ball b1{0.0, 0.0, 1.0, 1.0};
-    std::thread t1(play_ball, std::ref(b1));
-    std::thread t2(play_ball, std::ref(b1));
-    std::thread t3(play_ball, std::ref(b1));
+    std::vector<std::shared_ptr<BallVirt>> ball_vec;
+    ball_vec.push_back(std::make_shared<BallSimple>(1.0, 1.0, 0.0, 0.0, 1.0, 1.0));
+    ball_vec.push_back(std::make_shared<BallWind>(1.0, 1.0, 0.1, 0.0, 0.0, 1.0, 1.0));
+
+    Player p1{"dencer"};
+    Player p2{"player1"};
+    Player p3{"player2"};
+
+    std::thread t1(play_ball, std::ref(p1), std::ref(ball_vec));
+    std::thread t2(play_ball, std::ref(p2), std::ref(ball_vec));
+    std::thread t3(play_ball, std::ref(p3), std::ref(ball_vec));
     t1.join();
     t2.join();
     t3.join();
-    ASSERT_TRUE(std::abs(b1.x_ - 30.0) < eps);
+
+    std::vector<double> c1 = ball_vec[0]->get_coords();
+    std::vector<double> c2 = ball_vec[1]->get_coords();
+
+    std::cout << c1[0] << " " << c1[1] << std::endl;
+    std::cout << c2[0] << " " << c2[1] << std::endl;
+
+    ASSERT_TRUE(1.0);
 }
 
 int main(int argc, char** argv)
